857229264ab41136eeb7e55e59700cbcbc9d937e
jkolassa <jkolassa@cato.org>
2015-05-01 11:30:43 -0400

    Used preprocess_node to try and create template suggestions for the broadcast and print clips while in the daily_report view mode.
    Former-commit-id: e63affbbc0a4aaaec99cde923e8836a47a1c0606

--- sites/all/themes/dartik/template.php
+++ sites/all/themes/dartik/template.php
@@ -7,7 +7,8 @@
 function dartik_preprocess_node(&$vars) {
   if ($vars['node']->type == 'broadcast_clip' && $vars['view_mode'] == 'daily_report') {
     $vars['theme_hook_suggestions'][] = 'node__broadcast_clip__daily_report';
-  }
+  };
   if ($vars['node']->type == 'print_clip' && $vars['view_mode'] == 'daily_report') {
     $vars['theme_hook_suggestions'][] = 'node__print_clip__daily_report'; 
+  };
 }
