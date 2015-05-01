bfb09af10f64db6512c7256f2ea97cda9ea915ee
Jason Vines <jasonevines@gmail.com>
2015-05-01 14:16:44 -0400

    implemented field template suggestions for node view modes, and placed an example
    Former-commit-id: b5eef5e9ac5d29561744cdf515585b90704f3531

--- sites/all/themes/dartik/template.php
+++ sites/all/themes/dartik/template.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
-* Implements hook_preprocess_node().
-*/
-
-function dartik_preprocess_node(&$vars) {
-  if ($vars['node']->type == 'broadcast_clip' && $vars['view_mode'] == 'daily_report') {
-    $vars['theme_hook_suggestions'][] = 'node__broadcast_clip__daily_report';
-  };
-  if ($vars['node']->type == 'print_clip' && $vars['view_mode'] == 'daily_report') {
-    $vars['theme_hook_suggestions'][] = 'node__print_clip__daily_report'; 
-  };
+ * Implements hook_preprocess_field().
+ */
+function dartik_preprocess_field(&$vars) {
+  if (isset($vars['ds-config']['func'])) {
+    $suggestion = 'field__' . str_replace('theme_ds_field_', '', $vars['ds-config']['func']);
+  }
+  else {
+    $suggestion = 'field';
+  }
+  $vars['theme_hook_suggestions'][] = $suggestion . '__' . $vars['element']['#field_name'] . '__' . $vars['element']['#bundle'] . '__' . $vars['element']['#view_mode'];
 }
