9d53b256dddc80276b6debb9542acffa11471df0
jkolassa <jkolassa@cato.org>
2015-05-01 10:59:11 -0400

    Using preprocess_node function in order to get templates for the clips in the clips report field collection, via the daily_report view mode. #crossfingers
    Former-commit-id: 3c7a135729650bc06b7a0020ec7e049d482d7875

---
+++ sites/all/themes/dartik/template.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+* Implements hook_preprocess_node().
+*/
+
+function dartik_preprocess_node(&$vars) {
+  if ($vars['node']->type == 'broadcast_clip' && $vars['view_mode'] == 'daily_report') {
+    $vars['theme_hook_suggestions'][] = 'node__broadcast_clip__daily_report';
+  }
+  if ($vars['node']->type == 'print_clip' && $vars['view_mode'] == 'daily_report') {
+    $vars['theme_hook_suggestions'][] = 'node__print_clip__daily_report'; 
+}
