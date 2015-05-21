5d1af73732d88f43c5b221c82478ac27e7b70a8a
jkolassa <jkolassa@cato.org>
2015-05-21 15:36:52 -0400

    Cliptik theme, a child theme of Responsive Bartik for the Cato Clips System
    Former-commit-id: 5f91fdba297f086c6919cd8f7eb3dea69e661f8a

---
+++ sites/all/themes/cliptik/template.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Implements hook_preprocess_field().
+ */
+function cliptik_preprocess_field(&$vars) {
+  if (isset($vars['ds-config']['func'])) {
+    $suggestion = 'field__' . str_replace('theme_ds_field_', '', $vars['ds-config']['func']);
+  }
+  else {
+    $suggestion = 'field';
+  }
+  $vars['theme_hook_suggestions'][] = $suggestion . '__' . $vars['element']['#field_name'] . '__' . $vars['element']['#bundle'] . '__' . $vars['element']['#view_mode'];
+ }
+
+/**
+ * Implements hook_preprocess_node().
+ */
+
+/* function cliptik_preprocess_node(&$vars) {
+  $node = $vars['node'];
+  $view_mode = $vars['view_mode'];
+  if ($view_mode == 'daily_report') {
+    $field_attached_files = field_get_items('node', $vars['node'], 'field_attached_files');
+    $field_link = field_get_items('node', $vars['node'], 'field_link');
+    $field_primary = field_get_items('node', $vars['node'], 'field_primarysource');
+    if ($field_link){
+    	$field_primary['und'][0]['#prefix'] = '<a href="$field_link">';
+    	$field_primary['und'][0]['#suffix'] = '</a>';
+    }
+  }
+} */
+
+function cliptik_preprocess_node(&$vars) {
+  $node = $vars['node'];
+  $view_mode = $vars['view_mode'];
+  $content_type = $vars['content_type'];
+  if ($view_mode == 'daily_report' && $content_type = 'print_clip') {
+  	print dpm($vars['content']);
+  }
+}
