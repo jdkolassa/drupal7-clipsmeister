82a101f773bf2bcdee039dd7aae3dbf94c1e06d9
jkolassa <jkolassa@cato.org>
2015-05-28 09:36:00 -0400

    Foreign press nodequeue template, using the code to change "a" to "an" on vowels (but yet not working yet.)
    Former-commit-id: 04985c86ab0497c6614b1c1c7155dfbb07a669f5

---
+++ sites/all/themes/cliptik/templates/views-view-fields--nodequeue-5.tpl.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to all the fields as a row.
+ *
+ * - $view: The view in use.
+ * - $fields: an array of $field objects. Each one contains:
+ *   - $field->content: The output of the field.
+ *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
+ *   - $field->class: The safe class id to use.
+ *   - $field->handler: The Views field handler object controlling this field. Do not use
+ *     var_export to dump this object, as it can't handle the recursion.
+ *   - $field->inline: Whether or not the field should be inline.
+ *   - $field->inline_html: either div or span based on the above flag.
+ *   - $field->wrapper_prefix: A complete wrapper containing the inline_html to use.
+ *   - $field->wrapper_suffix: The closing tag for the wrapper.
+ *   - $field->separator: an optional separator that may appear before a field.
+ *   - $field->label: The wrap label text to use.
+ *   - $field->label_html: The full HTML of the label to use including
+ *     configured element type.
+ * - $row: The raw result object from the query, with all data it fetched.
+ *
+ * @ingroup views_templates
+ */
+?>
+
+
+<style>
+   .clip_entry div, .field-content {
+    display: inline;
+   }
+</style>
+
+<?php if (is_null($fields['field_custom_desc']->content)): ?>
+  <div class="clip_entry">
+  <?php print $fields['position']->content; ?>. <?php print $fields['field_primarysource']->content; ?></a>, <?php print $fields['field_publication_date']->content; ?>: <?php print $fields['field_scholar']->content; ?> <?php print $fields['field_cite']->content; ?> <?php if($fields['field_type']->raw === 'article' || $fields['field_type']->raw === 'op-Ed' || $fields['field_type']->raw === 'editorial') { ?> in an <?php print $fields['field_type']->content; } else { ?> in a <?php print $fields['field_type']->content; }?> <?php if(isset($fields['field_noncatoauths'])) { ?> by <?php print $fields['field_noncatoauths']->content; ?> <?php } ?> on <?php print $fields['field_subjects']->content; ?>: "<?php print $fields['field_headline']->content; ?>"<br><br>
+  </div>
+
+<?php else: ?>
+  <div class="clip_entry">
+  <?php print $fields['position']->content; ?>. <?php print $fields['field_primarysource']->content; ?> <?php if(isset($fields['field_othersources'])) { ?> (also appeared on <?php print $fields['field_othersources']->content; ?>) <?php } ?>, <?php print $fields['field_publication_date']->content; ?>: <?php print $fields['field_custom_desc']->content; ?><br><br>
+  </div>
+
+<?php endif; ?>
