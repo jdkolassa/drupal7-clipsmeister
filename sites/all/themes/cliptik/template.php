b9e91713fd6d9b3d90b7a5caae59500361a48623
Jason Vines <jasonevines@gmail.com>
2015-06-06 17:36:02 -0400

    wrap primary field in link
    Former-commit-id: df7996a3cd772876b51561c8481f4d45da461d0b

--- sites/all/themes/cliptik/template.php
+++ sites/all/themes/cliptik/template.php
@@ -12,17 +12,48 @@ function cliptik_preprocess_field(&$vars) {
 /**
  * Implements hook_preprocess_node().
  */
+function cliptik_preprocess_node(&$vars) {
+  _cliptik_link_primary($vars);
+}
 
-/* function cliptik_preprocess_node(&$vars) {
-  $node = $vars['node'];
-  $view_mode = $vars['view_mode'];
-  if ($view_mode == 'daily_report') {
-    $field_attached_files = field_get_items('node', $vars['node'], 'field_attached_files');
-    $field_link = field_get_items('node', $vars['node'], 'field_link');
-    $field_primary = field_get_items('node', $vars['node'], 'field_primarysource');
-    if ($field_link){
-    	$field_primary['und'][0]['#prefix'] = '<a href="$field_link">';
-    	$field_primary['und'][0]['#suffix'] = '</a>';
+/**
+ * Wraps the primary outlet or source in a link to either the clip's link or
+ * the taxonomy term page for the field
+ *
+ * @param $vars
+ */
+function _cliptik_link_primary(&$vars) {
+  if ($vars['view_mode'] == 'daily_report') {
+    switch ($vars['node']->type) {
+      case 'broadcast_clip':
+        $primary_field_name = 'field_primary_outlet';
+        break;
+
+      case 'print_clip':
+        $primary_field_name = 'field_primary_source';
+        break;
+    }
+    if (isset($primary_field_name)) {
+      if ($primary_field = field_get_items('node', $vars['node'],
+        $primary_field_name)) {
+        $field_link = field_get_items('node', $vars['node'], 'field_link');
+        if ($field_link){
+          $url = url($field_link[0]['url'], array(
+            'query' => $field_link[0]['query'],
+            'fragment' => $field_link[0]['fragment'],
+            'absolute' => TRUE,
+          ));
+        }
+        else {
+          $url = url('taxonomy/term/' . $primary_field[0]['tid'], array(
+            'absolute' => TRUE,
+          ));
+        }
+        $vars['content']['field_primary_outlet'][0]['#markup'] =
+          l($vars['content']['field_primary_outlet'][0]['#markup'], $url, array(
+            'html' => TRUE,
+          ));
+      }
     }
   }
-} */
+}
